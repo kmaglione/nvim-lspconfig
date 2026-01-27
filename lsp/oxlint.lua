d4f2dc71a6325b248b4db455166e0225d046bc2e
Kris Maglione <maglione.k@gmail.com>
2026-01-26 19:08:04 -0800

    feat(oxlint): Implement :LxpOxlintFixAll command in modern oxlint config
    Problem:
    The "oxc.fixAll" command is surfaced as a user command in the legacy
    config, but not the modern one.
    
    Solution:
    Implement :LspOxlintFixAll command in the modern config.

--- lsp/oxlint.lua
+++ lsp/oxlint.lua
@@ -8,6 +8,10 @@
 --- ```sh
 --- npm i -g oxlint
 --- ```
+---
+--- The default `on_attach` function provides an `:LspOxlintFixAll` command which
+--- can be used to fix all fixable diagnostics. See the `eslint` config entry for
+--- an example of how to use this to automatically fix all errors on write.
 
 local util = require 'lspconfig.util'
 
@@ -27,5 +31,18 @@ return {
     local fname = vim.api.nvim_buf_get_name(bufnr)
     local root_markers = util.insert_package_json({ '.oxlintrc.json' }, 'oxlint', fname)
     on_dir(vim.fs.dirname(vim.fs.find(root_markers, { path = fname, upward = true })[1]))
+  end,
+  on_attach = function(client, bufnr)
+    vim.api.nvim_buf_create_user_command(bufnr, 'LspOxlintFixAll', function()
+      client:exec_cmd({
+        title = 'Fix all Oxlint diagnostics',
+        command = 'oxc.fixAll',
+        arguments = {
+          {
+            uri = vim.uri_from_bufnr(bufnr),
+          },
+        },
+      }, { bufnr = bufnr })
+    end, { desc = 'Fix all Oxlint diagnostics' })
   end,
 }
